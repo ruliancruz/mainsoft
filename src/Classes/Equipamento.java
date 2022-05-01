53c8365ff8f58f6028bde78d6c631a75fffe8da5
Gessé Carneiro <carneiro.gesse@gmail.com>
2022-05-01 15:03:50 -0300

    adicionando class Equipamento

--- src/Classes/Equipamento.java
+++ src/Classes/Equipamento.java
@@ -1,5 +1,57 @@
 package Classes;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 class Equipamento {
-    ;
+    private String nome;
+    private String modelo;
+    private Date   dataDeAquisicao;
+    private Pecas  peca;
+    private ArrayList < ManutencaoAbstrata > manutencaosRealizadas = new ArrayList <ManutencaoAbstrata> ();
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataDeAquisicao() {
+        return dataDeAquisicao;
+    }
+
+    public void setDataDeAquisicao(Date dataDeAquisicao) {
+        this.dataDeAquisicao = dataDeAquisicao;
+    }
+
+    public ArrayList<ManutencaoAbstrata> getManutencaosRealizadas() {
+        return manutencaosRealizadas;
+    }
+
+    public void setManutencaosRealizadas(ArrayList<ManutencaoAbstrata> manutencaosRealizadas) {
+        this.manutencaosRealizadas = manutencaosRealizadas;
+    }
+
+    public Pecas getPeca() {
+        return peca;
+    }
+
+    public void setPeca(Pecas peca) {
+        this.peca = peca;
+    }
+    
+    
+    
+            
+    
 }
